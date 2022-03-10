f7bb5143e57e130b054ac9c04fb6c54ac1cce36b
Bishop Walker <bishop.walker@sbd2.com>
2022-03-10 18:08:22 -0500

    done

--- src/components/ViewSingleUser.js
+++ src/components/ViewSingleUser.js
@@ -9,14 +9,19 @@ const person = useSelector(state=>state.person[from])
 console.log(from,'from')
     console.log(person,'who')
     return(
-        <div className="App-header outSide ">
+        person.length!==0 ?
+    <div className="App-header outSide ">
 
-                <h2>Full Name: {person.firstName} {person.lastName}</h2>
-                <h2>Age: {person.age}</h2>
-                <h3>Hobbies: {person.hobbies}</h3>
+        <h2>Full Name: {person.firstName} {person.lastName}</h2>
+        <h2>Age: {person.age}</h2>
+        <h3>Hobbies: {person.hobbies}</h3>
+        <Link to='/addUser'>Return Back to Form</Link>
+
+    </div>:<div>
+            <h3>The List is Empty</h3>
                 <Link to='/addUser'>Return Back to Form</Link>
+            </div>
 
-        </div>
     )
 }
 export default ViewSingleUser
