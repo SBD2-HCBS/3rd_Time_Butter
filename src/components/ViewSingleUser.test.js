ad9f193986fa03ab470e77896f4fd1ab7f9544a8
Bishop Walker <bishop.walker@sbd2.com>
2022-03-15 17:47:42 -0400

    test for app

--- src/components/ViewSingleUser.test.js
+++ src/components/ViewSingleUser.test.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
  //import {render} from'../setupTests'
-import {render} from '@testing-library/react';
+import {render,screen} from '@testing-library/react';
 import ViewSingleUser from './ViewSingleUser';
 import userEvent from "@testing-library/user-event";
 import {createMemoryHistory} from 'history'
-import {MemoryRouter, useLocation} from 'react-router-dom'
+import {BrowserRouter, MemoryRouter, useLocation} from 'react-router-dom'
 import '@testing-library/jest-dom'
-import {Router, useSelector} from "react-redux";
+import { useSelector} from "react-redux";
 import store from '../ducks/store'
-
+import {Router, Route} from 'react-router'
+const renderWithRouter=(ui, {route='/'} = {}) =>{
+    window.history.pushState({}, 'Test Page', route)
+    return render(ui, {wrapper: BrowserRouter})
+}
+const history = createMemoryHistory()
+const renderWithRouter2=Component=> render(
+    <Router history={history}>
+        <Route component={Component}/>
+    </Router>
+)
 jest.mock("react-router-dom",()=>({
     ...jest.requireActual("react-router-dom"),
     useLocation:() => ({
-        pathname: 'localhost:3000/viewSingleUser'
+        pathname: 'localhost:3000/viewSingleUser',
     })
 
 }))
@@ -52,24 +62,32 @@ describe('View Single User Component', () => {
 
 
     }
-    test('should not render if person length is zero',()=>{
-        const history = createMemoryHistory()
-        render(<Router history={history}><ViewSingleUser from={location} path='/viewSingleUser' location={loco}/></Router>)
-        expect(screen.getByText(/viewsingleuser/i)).toBeInTheDocument();
+    // test('should not render if person length is zero',()=>{
+    //     const history = createMemoryHistory()
+    //     render(<ViewSingleUser from={location} path='/viewSingleUser' location={loco}/>,{wrapper: MemoryRouter})
+    //     expect(screen.getByText(/Full Name/i)).toBeInTheDocument();
+    //
+    // })
+    test('render',()=>{
+        // renderWithRouter(<ViewSingleUser/>)
+        // expect(screen.getByText(/Full Name/i)).toBeInTheDocument();
+        const render = () => renderWithRouter(ViewSingleUser)
+        console.log(render)
+        expect(screen.getByText(/Return Back to Form/i)).toBeInTheDocument()
+    })
+    test('render something else',()=>{
 
     })
 
-
-
-    // test('app',()=>{
-    //     const history = createMemoryHistory()
-    //     render(
-    //         <Router path='/ViewSingleUser' history={history}>
-    //             <ViewSingleUser person={person} location={loco} />
-    //         </Router>
-    //     )
-    //     expect(screen.getByText('Hobbbies')).toBeInTheDocument();
-    // })
+    test('app',()=>{
+        const history = createMemoryHistory()
+        render(
+            <Router path='/ViewSingleUser' history={history}>
+                <ViewSingleUser person={person} location={loco} />
+            </Router>
+        )
+        expect(screen.getByText('Hobbbies')).toBeInTheDocument();
+    })
 
 
 })
