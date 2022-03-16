a76cd42544bddf596ba0942461dee40e8aa02f98
Bishop Walker <bishop.walker@sbd2.com>
2022-03-16 18:10:18 -0400

    added MapStateToProps

--- src/components/ViewSingleUser.js
+++ src/components/ViewSingleUser.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import {useLocation,Link} from 'react-router-dom'
 import {useSelector}  from "react-redux";
-
-const ViewSingleUser=()=>{
+import {connect} from 'react-redux'
+const ViewSingleUser=(props)=>{
     const location = useLocation()
     const {from} = location.state
-const person = useSelector(state=>state.person[from])
-
+//const person = useSelector(state=>state.person[from])
+const people=React.useRef(props.person[from])
     const isObjectEmpty=(obj={})=>{
         return Object.keys(obj).length === 0
     }
     return(
-       !isObjectEmpty(person) ?
+       !isObjectEmpty(people.current) ?
     <div className="App-header outSide ">
 
-        <h2>Full Name: {person.firstName} {person.lastName}</h2>
-        <h2>Age: {person.age}</h2>
-        <h3>Hobbies: {person.hobbies}</h3>
+        <h2>Full Name: {people.current.firstName} {people.current.lastName}</h2>
+        <h2>Age: {people.current.age}</h2>
+        <h3>Hobbies: {people.current.hobbies}</h3>
         <Link to='/addUser'>Return Back to Form</Link>
         <br></br>
         <Link to='/viewContainer'>
@@ -30,4 +30,7 @@ const person = useSelector(state=>state.person[from])
 
     )
 }
-export default ViewSingleUser
+const mapStateToProps = state => ({
+    person:state.person
+})
+export default connect(mapStateToProps,null)(ViewSingleUser)
